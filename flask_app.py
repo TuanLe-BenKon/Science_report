f2b67d092193e13530194471c7d5e31d3b388015
nhat-thai <nhat.thai@lab2lives.com>
2022-04-08 10:29:47 +0700

    test without summary chart

--- flask_app.py
+++ flask_app.py
@@ -85,12 +85,12 @@ def dailyReport():
     if user_id in ["10019", "11294", "11296", "10940"]:
         mail_list = ["nhat.thai@lab2lives.com", "thomas.luu@lab2lives.com"]
         bcc_list = [
-            'tuan.le@lab2lives.com',
-            'hieu.tran@lab2lives.com',
-            'taddy@lab2lives.com',
-            'liam.thai@lab2lives.com',
-            'dung.bui@lab2lives.com',
-            'ann.tran@lab2lives.com'
+            # 'tuan.le@lab2lives.com',
+            # 'hieu.tran@lab2lives.com',
+            # 'taddy@lab2lives.com',
+            # 'liam.thai@lab2lives.com',
+            # 'dung.bui@lab2lives.com',
+            # 'ann.tran@lab2lives.com'
         ]
     else:
         records = get_customer_emails()
